43da8c077b57b6b4d881c26eac30914751e9468d
Aline Senault <senault.aline@laposte.net>
2021-06-21 14:20:13 +0200

    ajout de la fonctionnalite couleur

--- script.js
+++ script.js
@@ -3,23 +3,24 @@ const btnCancel = document.querySelectorAll('button')[1]
 const form = document.querySelector('form')
 const input1 = document.querySelectorAll('input')[0]
 const input2 = document.querySelectorAll('input')[1]
+const input3 = document.querySelectorAll('input')[2]
 const section = document.querySelector('section')
 
 // 1ere fonctionnalite
 
 btnCancel.addEventListener('click', function () {
-    input1.value = ""
-    input2.value = ""
+  input1.value = ""
+  input2.value = ""
 })
 // 2eme fonctionnalite
 
 form.addEventListener('submit', function (evt) {
-    evt.preventDefault()
+  evt.preventDefault()
 
-    $('section').append(`
+  $('section').append(`
     <div class="card mt-3">
       <div class="card-body">
-        <p class="card-text">${input2.value} - ${input1.value}</p>
+        <p class="card-text"><span style = "color:${input3.value}">${input2.value}</span>- ${input1.value}</p>
       </div>
     </div>
     `)
